a8cfa826be1d1a2dce4b4224f2cddf5fc3abffc2
Ariel Herrera Fernández <carherrera@outlook.es>
2015-09-16 01:24:16 -0600

    Aplicación
    Funciones básicas de usuarios y otros.

---
+++ AtleticChampionship/AtleticChampionship/Account/Register.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using AtleticChampionship.Models;
+using Microsoft.SqlServer.Server;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+
+namespace AtleticChampionship.Account
+{
+    public partial class Register : Page
+    {
+        private static SqlCommand SQLcommand = new SqlCommand();
+        protected void CreateUser_Click(object sender, EventArgs e)
+        {
+            SQLcommand.CommandText = "addUsuario";
+            SQLcommand.CommandType = CommandType.StoredProcedure;
+            SQLcommand.Parameters.Add("@pCorreo", SqlDbType.NVarChar).Value = Email.Text;
+            SQLcommand.Parameters.Add("@pPassword", SqlDbType.NVarChar).Value = Password.Text;
+            SQLcommand.Parameters.Add("@pUsername", SqlDbType.NVarChar).Value = Username.Text;
+            SQLcommand.Parameters.Add("@pApellido", SqlDbType.NVarChar).Value = Apellido.Text;
+            SQLcommand.Parameters.Add("@pNombre", SqlDbType.NVarChar).Value = Nombre.Text;
+            SQLcommand.Parameters.Add("@pCedula", SqlDbType.Int).Value = Cedula.Text;
+            SQLcommand.Parameters.Add("@pCodigo", SqlDbType.NVarChar).Value = Codigo.Text;
+            SQLcommand.Parameters.Add("@pTelefono", SqlDbType.Int).Value = Telefono.Text;
+            SqlParameter outputValue = new SqlParameter("@pEstado", SqlDbType.Int);
+            outputValue.Value = 0;
+            outputValue.Direction = ParameterDirection.InputOutput;
+            SQLcommand.Parameters.Add(outputValue);
+
+            connectionDB.writeTransaction(SQLcommand);
+            Cedula.Text = "";
+            Nombre.Text = "";
+            Apellido.Text = "";
+            Email.Text = "";
+            Username.Text = "";
+            if (((int)outputValue.Value) == 1)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "alert" + UniqueID, "alert('El Registro fue completado exitosamente.');", true);
+            }
+            else
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "alert" + UniqueID, "alert('Error al crear el nuevo usuario, verifique que todos los datos sean validos.');", true);
+            }
+        }
+    }
+}
---
+++ AtleticChampionship/AtleticChampionship/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AtleticChampionship.Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="separatorContainer">
+        <div class="container containerPaddingBottom containerOpacity containerColor separatorHeader">
+            <div class="row">
+                <div class="col-md-4">
+                    <img src="Images/logo2.png" runat="server" class="containerCustom alignImage" />
+                </div>
+                <div class="col-md-7">
+                    <div class="row">
+                        <div class="containerCustom containerDifferentialColor containerColor">
+                            <h1 class="text-center">Asociación internacional de federaciones de Atletismo</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="containerCustom containerDifferentialColor">
+                            <h3 class="text-center">Bienvenido a la casa del atletismo mundial</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
